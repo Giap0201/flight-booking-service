eadee567fec0b293f2e799361a6d774dd662ea94
Nguyễn Hữu Giáp <giap13022005@gmail.com>
2026-02-13 12:15:55 +0700

    refactor(excetion): refactor globalxepceptionhandle

--- src/main/java/com/utc/flight_booking_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/utc/flight_booking_service/exception/GlobalExceptionHandler.java
@@ -3,50 +3,56 @@ package com.utc.flight_booking_service.exception;
 import com.utc.flight_booking_service.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    // Bat cac loi chua dinh nghia
-    @ExceptionHandler(value = RuntimeException.class)
-    ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException e) {
+    // Bat toan bo loi chua duoc dinh nghia
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error(e.getMessage(), e);
         ApiResponse<?> apiResponse = ApiResponse.builder()
                 .code(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode())
                 .message(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage())
                 .build();
-        log.error(e.getMessage(), e);
-        return ResponseEntity.status(ErrorCode.UNCATEGORIZED_EXCEPTION.getHttpStatusCode()).body(apiResponse);
+        return ResponseEntity.status(ErrorCode.UNCATEGORIZED_EXCEPTION
+                .getHttpStatusCode()).body(apiResponse);
     }
 
-    // Bat cac loi tu dinh nghia
+    // Bat cac loi da duoc dinh nghia trong ErrorCode
     @ExceptionHandler(value = AppException.class)
     ResponseEntity<ApiResponse<?>> handleAppException(AppException e) {
-        ErrorCode errorCode = e.getErrorCode();
+        log.error(e.getMessage(), e);
         ApiResponse<?> apiResponse = ApiResponse.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
+                .code(e.getErrorCode().getCode())
+                .message(e.getErrorCode().getMessage())
                 .build();
-        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(apiResponse);
+        return ResponseEntity.status(e.getErrorCode()
+                .getHttpStatusCode()).body(apiResponse);
     }
 
-    // Bat loi khong trung message trong dto
+    // Bat loi validate DTO khong hop le
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String enumKey = e.getBindingResult().getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
-        try {
-            errorCode = ErrorCode.valueOf(enumKey);
-        }catch (IllegalArgumentException illegalArgumentException) {
-            log.warn("Invalid value for key {}", enumKey);
+        FieldError fieldError = e.getBindingResult().getFieldError();
+        if (fieldError != null) {
+            String enumKey = fieldError.getDefaultMessage();
+            try {
+                errorCode = ErrorCode.valueOf(enumKey);
+            } catch (IllegalArgumentException ex) {
+                log.warn("Lỗi Validate: Không tìm thấy ErrorCode nào ứng với key {}", enumKey);
+            }
         }
         ApiResponse<?> apiResponse = ApiResponse.builder()
                 .code(errorCode.getCode())
                 .message(errorCode.getMessage())
                 .build();
-        return ResponseEntity.status(errorCode.getHttpStatusCode()).body(apiResponse);
+        return ResponseEntity.status(errorCode
+                .getHttpStatusCode()).body(apiResponse);
     }
 }
